db3e4d11f00a155398241764696ec21cdf5dc5e9
liuyongfei <liuyongfei198805@gmail.com>
2017-08-17 11:08:47 +0800

    使用插件压缩js代码

--- app/main.js
+++ app/main.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import {render} from 'react-dom';
 import Greeter from './Greeter';
 
-import './main.css';//使用require导入css文件
+// import './main.css';//使用require导入css文件
 
-render(<Greeter />, document.getElementById('root'));
+// render(<Greeter/>, document.getElementById('root'));
+
+ReactDOM.render(<Greeter/>, document.getElementById('root')
+)
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,29 +1,26 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: __dirname + "/app/main.js" , // 唯一入口文件
+  entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
   output: {
-    path: __dirname + "/build", // 打包后的文件存放的地方
-    filename: "bundle.js" // 打包后输出的文件名
+    path: __dirname + "/build",
+    filename: "bundle.js"
   },
   devtool: 'eval-source-map',
   devServer: {
     contentBase: "./public",//本地服务器所加载的页面所在的目录
     historyApiFallback: true,//不跳转
-    inline: true//实时刷新
+    inline: true,
+    hot: true
   },
   module: {
     rules: [
       {
-        test: /(\.jsx|\.js)$/, // 一个用以匹配loaders所处理文件的拓展名的正则表达式（必须）
+        test: /(\.jsx|\.js)$/,
         use: {
-          loader: "babel-loader", // loader的名称（必须）
-          options: {
-            presets: [
-              "es2015", "react"
-            ]
-          }
+          loader: "babel-loader"
         },
         exclude: /node_modules/
       },
@@ -37,6 +34,15 @@ module.exports = {
             options: {
               modules: true
             }
+          }, {
+            loader: "postcss-loader",
+            options: {
+              plugins: function() {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
           }
         ]
       }
@@ -47,6 +53,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
     }),
-    new webpack.HotModuleReplacementPlugin()//热加载插件
-  ]
-}
+    new webpack.HotModuleReplacementPlugin(),//热加载插件,
+    // new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      sourceMap: true
+    }),
+    // new ExtractTextPlugin("style.css")
+  ],
+};
